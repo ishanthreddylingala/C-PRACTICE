cb7851d8d3e11dc124babe5d8c45c549b029cd11
ISHANTH REDDY LINGALA <ishanthlingala@gmail.com>
2026-06-22 23:37:48 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,396 @@
+// #include<stdio.h> 
+
+// int main() {
+//     int a = 2;
+//     printf("%d \n", a);
+//     printf("%p", &a);
+
+//     return 0;
+// }
+
+// int main() {
+
+//     int age = 24234;
+//     printf("age is %d " ,age);
+//     return 0;
+
+
+// int main() {
+
+//     printf("age is 22");
+// // //     return 0;
+// int main() {
+
+//     int age;
+//     printf("enter your age");
+//     scanf("%d", &age);
+//     printf("age is : %d",age);
+//     return 0;
+// }
+
+// int main() {
+
+//     int side;
+//     printf("enter side");
+//     scanf("%d", &side);
+//     printf("area is %d", side*side);
+//     return 0;
+// // }
+
+
+// int main() {
+
+// float radius;
+// printf("enter radius");
+// scanf("%f" , &radius);
+// printf("area is : %f", 3.14 * radius * radius);
+// return 0;   
+// }
+
+
+
+// int main() {
+
+//  int a,b;
+//  printf("enter values :  ");
+//  scanf("%d %d" , &a ,&b);
+
+//   int sum = a+b;
+
+//  printf("answer is %d",sum);
+//  return 0;
+
+// }
+
+
+// int main() {
+
+//     float side;
+//     printf("enter side value is : ");
+//     scanf("%f", &side);
+
+//     double area;
+//     area = side*side;
+
+//     printf("area of square is %f", area);
+
+
+//     return 0;           
+// // }
+
+
+// int main() {
+
+//    int side;
+//    printf("enter the side \n");
+//    scanf("%d" ,&side);
+//    int area=side*side;
+//    printf("area of square with side is %d" ,area);
+//    return 0; 
+// // }
+
+// #include<math.h>
+
+// int main() {
+
+//     int b,c;
+//     printf("enter the value of b,c");
+//     scanf("%d" "%d" , &b , &c);
+//      int power =(int) pow(b,c);
+//      printf("answer is %d" , power);
+// //     return 0;
+// // }
+
+// int main() {
+
+// int a,b,remainder;
+// printf("enter the value of a : \n");
+// printf("enter the value of b : \n");
+// scanf("%d"  "%d" , &a, &b );
+// remainder = a % b;
+// // printf("answer is %d " , remainder);
+// // return 0;
+// // }
+
+// // int main() {
+// //     printf("%f" , 2/3.0);
+// //      return 0;
+// // }
+
+// int main() {
+
+// //     int x;
+// //     printf("enter the value of x :");
+// //     scanf("%d" , &x);
+// //     x%2=y;
+// //     printf("%d" ,y=0 );
+// //     return 0;
+// // // }
+
+
+
+#include<stdio.h>
+
+// int main () {
+
+// int marks;
+// if (marks>0 && marks <= 30){
+//     primtf("fail");
+
+//     else if (marks >30 && marks< 100);
+//     printf("pass")
+// }
+// else {
+//     printf("erorr");
+
+// }
+// return 0;
+// // }
+
+// int main() {
+
+//     printf("hello world ");
+//     return 0;
+// }
+
+// int main() {
+//      int age;
+//     printf("netr the age :");
+//     scanf("%d" , &age);
+
+//     age>=18 ? printf("you are adult") : printf("you are not a adult \n ");
+//      return 0;
+// }
+
+// int main () {
+ 
+//     int x;
+//     printf("enter the value:");
+//     scanf("%d" , &x);
+    
+//     printf("answwer is %d" , x%2==0);
+//     return 0;
+// }
+
+    // int main () {
+    //     for(int i=3 ; i<=9 ; i=i+7) {
+    //         printf("hello");
+    //     }
+    //     return 0;
+    // // }
+    
+
+    // int main() {
+
+    //     printf("heo"); 
+//     //     return 0;
+//     // }
+
+// int main() {
+
+//     int i=1;
+//     printf("%d \n" , ++i );
+//     return 0;
+// }
+
+// int main() {
+
+//      for(int i=1; i <=6 ; i++){
+//         printf("%d \n" , i);
+
+//      }
+//      return 0;
+// // }
+
+
+// int main() {
+//     char ch='a';
+//     while(ch++)
+//     {
+//         printf("%c \n " , ch);
+
+//     }
+//     return 0;
+// }
+
+// int main() {
+
+//     int n;
+//     printf("enter the value of n \n");
+//     scanf("%d" , &n);
+//     for(int i=0 ; i<n+1 ; i++){
+//         printf("%d \n" , i);
+//     }
+// //     return 0;
+// //     }
+
+//     int main() {
+
+//         int n;
+//     //     printf("enter the value of n :");
+//     //     scanf("%d" , &n);
+
+//     //     int sum=0;
+        
+    //     for(int i=0; i<n+1; i++){
+
+    //         sum = sum+i;
+
+    //     }
+
+    //     printf("%d", sum);
+
+
+    // return 0;
+//    /
+
+    // int main() {
+    //     int n;
+    //     printf("n :::");
+    //     scanf("%d", &n);
+    //     for(int i=0; i<=10; i++){
+    //     printf("%d \n", i*n);
+            
+
+    // //     }
+    
+    //     int main() {
+    //     int n;
+    //         printf("n:::");
+    //         scanf("%d" , &n);
+
+    //         for(int i=0; i<n; i++ ){ 
+    //         if(i==4){
+    //             break;
+    //         }
+    //         printf("%d", i);
+    //         }
+    //         printf("the end");
+    //         return 0;
+    //     }
+
+
+// // int main() {
+// //     int n;
+
+// //     for(;;) {              // infinite loop
+// //         printf("enter number : ");
+// //         scanf("%d", &n);
+// //         printf("%d\n", n);
+
+// //         if(n % 2 != 0) {
+// //             break;
+// //         }
+// //     }
+
+// //     printf("thank you");
+
+// //     return 0;
+// // }
+
+// // int main() {
+// //  int n;
+// //     for(;;) {
+// //          printf("n::::");
+// //          scanf("%d" , &n);
+// //          if(n%7 !=0){
+// //             break;
+// //          }
+// //     }
+// //     printf("THE END");
+// //     return 0;
+// // }
+
+
+// int main() {
+//      for(int i=0; i<=7; i++){
+        
+//         if(i==4){
+//             continue;
+//         }
+//         printf("%d \n", i);
+
+//      }
+//      return 0; 
+    // }
+
+        // int main() {
+            
+        //     for(int i=5; i<51; i++){
+        //         if(i%2 !=0){
+        // printf("%d \n", i);
+        //         }
+            
+        //     }
+        //     return 0;
+        // }
+
+        // int main() {
+        //      for(int i=0; ;i++){
+        //         printf("%d \n" , i);
+        //      }
+        
+        // return 0;
+        // }
+
+// FACTORIAL 
+
+
+    // int main() {
+
+    //     int fac=1,n ;
+    //     printf("n");
+    //     scanf("%d" , &n);
+
+    //     for(int i=1; i<=n; i++){ 
+    //         fac= fac * i;
+    //     }
+    //         printf("%d" , fac);
+
+    //     return 0;
+    // }
+
+    //table of numbeer in reverse order
+
+    // int main() {
+    //     int n;
+    //     printf("n::::");
+    //     scanf("%d", &n);
+    //     for(int i=10; i>=1; i-- ){
+    //         printf("%d \n ", i*n);
+
+    //     }
+    //     return 0;
+
+    // }
+
+//SUM OF ALL NUMBERS INBTW  6 AND 51
+
+    //     int main() {
+    //     int sum=0;
+    //         for(int i=5; i<51; i++){
+    //         sum= sum+i;
+           
+    //         }
+    //          printf("%d \n", sum);
+    // return 0;
+    //     }
+ 
+    //IS A NUMBER A PRIME OR NOT
+
+    void printname();
+
+    int main() {
+        printname() ;
+        return 0;
+    }
+
+       void printname() {
+        printf("shadow fighter \n");
+       }
+
+        
+
+    
+
+        
